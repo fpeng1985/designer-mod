792cafbb2caac33263a2593ad2ed9712a1c308bd
Fei <fpeng1985@126.com>
2016-11-18 22:51:11 +0800

    refine main_pf.c

--- src/main_pf.c
+++ src/main_pf.c
@@ -3,34 +3,21 @@
 //
 
 #include <stdio.h>
-#include <string.h>
 #include <glib.h>
 #include "fileio.h"
-#include "design.h"
-#include "graph_dialog.h"
 #include "global_consts.h"
-#include "simulation.h"
-#include "simulation_data.h"
-#include "coherence_vector.h"
-#include "graph_dialog_widget_data.h"
-#include "bistable_simulation.h"
-
 #include "objects/QCADDOContainer.h"
-#include "objects/QCADLayer.h"
-#include "objects/QCADCell.h"
-#include "exp_array.h"
 
 
 int main(int argc, char **argv) {
 
+    //[1]create design
     QCADSubstrate *sub = NULL;
     DESIGN *design = NULL;
     design = design_new(&sub);
 
-    VectorTable *pvt = NULL;
-
-    QCADLayer *layer = QCAD_LAYER(g_list_first(design->lstLayers->data));
-
+    //[2]insert cells into "Main Cell Layer" of the design
+    QCADLayer *layer = QCAD_LAYER(g_list_first(design->lstLayers)->data);
     QCADCell *cell = NULL;
 
     cell = QCAD_CELL(qcad_cell_new(200, 200));
@@ -56,20 +43,27 @@ int main(int argc, char **argv) {
     qcad_cell_set_label(cell, "C");
     qcad_do_container_add(QCAD_DO_CONTAINER(layer), QCAD_DESIGN_OBJECT(cell));
 
+    //[3]define vector table variable
+    VectorTable *pvt = NULL;
     VectorTable_add_inputs(pvt, design);
 
-    gchar *design_file  = "c:\\Users\\fpeng\\Desktop\\qcad\\layer.qca";
-    create_file(design_file, design);
-
+    //[4]run simulation, and construct the corresponding simulation output data structure
     simulation_data *sim_data = run_simulation(BISTABLE, EXHAUSTIVE_VERIFICATION, design, pvt);
-
     SIMULATION_OUTPUT sim_output = {sim_data, design->bus_layout, FALSE};
 
-    gchar *output_file = "c:\\Users\\fpeng\\Desktop\\qcad\\sim_output";
+    //[5] generate qca ${HOME}/design.qca and simulation output file ${HOME}/sim_output.txt
+    GString *home = NULL;
+
+    home = g_string_new(getenv("HOME"));
+    gchar *design_file  = (g_string_append(home, "\\design.qca"))->str;
+    create_file(design_file, design);
+
+    home = g_string_new(getenv("HOME"));
+    gchar *output_file = (g_string_append(home, "\\sim_output.txt"))->str;
     create_simulation_output_file(output_file, &sim_output);
 
+    //[6]destroy design object, reclaim its memory
     design_destroy(design);
-    VectorTable_free(pvt);
 
     return 0;
 }
