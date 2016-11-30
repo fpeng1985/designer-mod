3e83a2a7766a893fd63f0cbdafab93893860d20d
Fei <fpeng1985@126.com>
2016-12-01 02:00:50 +0800

    refine os.environ[PATH] setting in sim_manager.py

--- qcamod/sim_manager.py
+++ qcamod/sim_manager.py
@@ -3,18 +3,24 @@
 
 import argparse
 import os
+import platform
 
 from generate_simulation_files import generate_structures_from_benchmark, generate_qca_and_sim_from_structure
 from generate_logic_files import generate_truth_from_sim, generate_logic_from_truth
 
 ###############################################################################
+#program settings
 
-default_benchmark_file_name = r"C:\msys64\common\benchmark\qcasim\majority_gate_1.txt"
-default_output_dir =r"C:\Users\fpeng\Documents\sim_manager"
+default_benchmark_file_name = ""
+default_output_dir = ""
 
-parser = argparse.ArgumentParser(description="Specify the benchmark file")
-parser.add_argument("-i", type=str, nargs='?', dest='benchmark_file_name', default=default_benchmark_file_name, help='benchmark file name')
-parser.add_argument("-o", type=str, nargs='?', dest='outdir', default=default_output_dir, help='output file directory')
+if platform.system() == "Windows":
+    default_benchmark_file_name = r"C:\msys64\common\benchmark\qcasim\majority_gate_1.txt"
+    default_output_dir =r"C:\Users\fpeng\Documents\sim_manager"
+    
+    path_env = [r"C:\msys64\mingw64\bin", r"C:\msys64\mingw64\lib", r"C:\Users\fpeng\Data\Workspace\HFUT\QCADesigner\build\bin"]
+    for p in path_env :
+        os.environ["PATH"] += (p+";")
 
 ###############################################################################
 
@@ -29,9 +35,11 @@ def visit_outdir(outdir, func, appendix):
         for needed_file_name in needed_file_names:
             func(os.path.abspath(needed_file_name), os.path.abspath(output_dir))
 
-###############################################################################
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Specify the benchmark file")
+    parser.add_argument("-i", type=str, nargs='?', dest='benchmark_file_name', default=default_benchmark_file_name, help='benchmark file name')
+    parser.add_argument("-o", type=str, nargs='?', dest='outdir', default=default_output_dir, help='output file directory')
     args = parser.parse_args()
     
     #make output files dir
